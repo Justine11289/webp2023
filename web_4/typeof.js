09d86da8a605fb7c504cfc2655d9e4fe3ecfd215
Justine11289 <91184073+Justine11289@users.noreply.github.com>
2023-03-15 15:29:32 +0800

    0315

---
+++ web_4/typeof.js
@@ -0,0 +1,7 @@
+console.log(typeof 37);
+console.log(typeof NaN);
+console.log(typeof '');
+console.log(typeof (typeof 1));
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof function(){});
