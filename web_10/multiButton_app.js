110d9f821547eab56676fc06cb8896359ea81d5a
Justine11289 <91184073+Justine11289@users.noreply.github.com>
2023-05-10 15:34:58 +0800

    0503practice1

---
+++ web_10/multiButton_app.js
@@ -0,0 +1,12 @@
+import './App.css';
+import multiButton from './cgu_multiButton';
+
+function App() {
+  return (
+    <div className="App">
+      {multiButton(10)}
+    </div>
+  );
+}
+
+export default App;
